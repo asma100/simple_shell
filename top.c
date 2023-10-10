2432afa935de1570c0fbdfe3e5929e89ba470b5e
0xsuda <osama.suliman@protonmail.com>
2023-10-09 19:02:27 -0700

    memory free update

--- top.c
+++ top.c
@@ -14,55 +14,87 @@ return (cou);
 }
 int main(void)
 {
-  char *input = NULL;
-  ssize_t checkline;
-  size_t s = 0;
-  char *input_cp = NULL;
-  char *tok;
-  int tok_counter =0;
-  char **argv;
+    char *input = NULL;
+    ssize_t checkline;
+    size_t s = 0;
+    char *input_cp = NULL;
+    char *tok;
+    int tok_counter = 0;
+    char **argv = NULL;
     int u = 0;
-const char *delim = " \n";
-  while (1)
-  {
-print_top("top$");
-checkline = getline(&input, &s, stdin);
-if(checkline == -1)
- {  
-return (-1); 
-}
-input_cp = malloc(sizeof(char) * checkline);
-if (input_cp == NULL){
-return (-1);
-}
- strcpy(input_cp,input);
- if (checkline == -1){
-print_top("Exiting shell....\n");
-return (-1);
-}
-else {
-tok = strtok(input, delim);
-while (tok != NULL){
-tok_counter++;
-tok = strtok(NULL, delim);
-}
-tok_counter++;
-argv = malloc(sizeof(char *) * tok_counter);
-tok = strtok(input_cp, delim);
-for (u=0; tok != NULL; u++){
-argv[u] = malloc(sizeof(char) * strlen(tok));
-strcpy(argv[u], tok);
-tok = strtok(NULL, delim);
+    int j;
+    const char *delim = " \n";
+
+    while (1)
+    {
+        print_top("top$");
+        checkline = getline(&input, &s, stdin);
+        if (checkline == -1)
+        {
+            perror("Error reading input:");
+            free(input);
+            free(input_cp);
+            free(argv); 
+            return (-1);
         }
-argv[u] = NULL;
-topcmd(argv);
-if (access("usr/bin/input", F_OK) != 0){
-print_top("Command does not exist.\n");
-}
-}
-}
-free(argv); 
-free(input);
-free(input_cp);
-  return (0);
+
+        input_cp = malloc(sizeof(char) * (checkline + 1));
+        if (input_cp == NULL)
+        {
+            perror("Error allocating memory:");
+            free(input);
+            free(argv); 
+            return (-1);
+        }
+        strcpy(input_cp, input);
+
+        tok = strtok(input, delim);
+        while (tok != NULL)
+        {
+            tok_counter++;
+            tok = strtok(NULL, delim);
+        }
+        tok_counter++;
+        
+        argv = malloc(sizeof(char *) * tok_counter);
+        if (argv == NULL)
+        {
+            perror("Error allocating memory for argv:");
+            free(input);
+            free(input_cp);
+            return (-1);
+        }
+        
+        tok = strtok(input_cp, delim);
+        for (u = 0; tok != NULL; u++)
+        {
+            argv[u] = malloc(sizeof(char) * (strlen(tok) + 1));
+            if (argv[u] == NULL)
+            {
+                perror("Error allocating memory for argv[u]:");
+                for (j = 0; j < u; j++)
+                {
+                    free(argv[j]);
+                }
+                free(argv);
+                free(input);
+                free(input_cp);
+                return (-1);
+            }
+            strcpy(argv[u], tok);
+            tok = strtok(NULL, delim);
+        }
+        argv[u] = NULL;
+        topcmd(argv);
+        
+        for (u = 0; u < tok_counter; u++)
+        {
+            free(argv[u]);
+        }
+        free(argv);
+    }
+
+    free(input);
+    free(input_cp);
+    return (0);
 }
