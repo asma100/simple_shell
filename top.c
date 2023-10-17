005b6e73124ab52b8f491d753fc8968f4ec94270
asma <asmaa.alhadi100@gmail.com>
2023-10-17 15:48:40 -0700

    edi

--- top.c
+++ top.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "top.h"
+size_t _strlen(const char *s) {
+ int cou = 0;
+ while (*s != '\0') {
+ s++;
+ cou++;
+ }
+ return (cou);
+}
 
+void print_error(const char *message) {
+  size_t len = strlen(message);
+  write(STDERR_FILENO, message, len);
+
+}
 
 
 int main(void) {
