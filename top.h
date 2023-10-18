3c88d18218484f559ac470ac7bca28c14448d87a
asma <asmaa.alhadi100@gmail.com>
2023-10-18 11:49:10 -0700

    h files

--- top.h
+++ top.h
@@ -8,7 +8,9 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <errno.h>
-extern char **environ;
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 void print_top(char *top);
 void topcmd(char **argv);
 void Parse(char *input, const char *delim);
@@ -18,6 +20,7 @@ void env_builtin();
 void print_error(const char *message);
 size_t _strlen(const char *s);
 void parsetok(char *input_cp, char *input);
+extern char **environ;
 /*void read_command(char *command, size_t size);
   void execute_command(const char *command);*/
 
--- topexec.c
+++ topexec.c
@@ -1,43 +1,73 @@
 #include "top.h"
 void topcmd(char **argv)
 {
-char *cmd = NULL, *acmd = NULL;
-pid_t pid;
-if (argv)
-{
-cmd = argv[0];
-acmd = finding_path(cmd);
-if (acmd == NULL)
-{
-perror("Error:");
-return;
-}
-pid = fork();
-if (pid == -1)
-{
-perror("Error:");
-}
-else if (pid == 0)
-{
-if (strcmp(cmd, "env") == 0)
-{
-env_builtin();
-}
-else if (execve(acmd, argv, NULL) == -1)
-{
-perror("Error:");
-exit(EXIT_FAILURE);
-}
-}
-else
-{
-int status;
-if (waitpid(pid, &status, 0) == -1)
-{
-perror("Error:");
-}
-}
-}
-if (cmd != acmd)
-free(acmd);
+    char *cmd = NULL, *acmd = NULL;
+    pid_t pid;
+    int fd_in, fd_out, i;
+
+    if (argv)
+    {
+        cmd = argv[0];
+        acmd = finding_path(cmd);
+        if (acmd == NULL)
+        {
+            perror("Error:");
+            return;
+        }
+
+        /*check for input/output redirection symbols*/
+        for ( i = 1; argv[i] != NULL; i++) {
+            if (strcmp(argv[i], "<") == 0) {
+                /* redirect input from file*/
+                fd_in = open(argv[i+1], O_RDONLY);
+                if (fd_in == -1) {
+                    perror("Error:");
+                    return;
+                }
+                dup2(fd_in, STDIN_FILENO);
+                close(fd_in);
+                argv[i] = NULL;
+                argv[i+1] = NULL;
+            } else if (strcmp(argv[i], ">") == 0) {
+                /* redirect output to file*/
+                fd_out = open(argv[i+1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+                if (fd_out == -1) {
+                    perror("Error:");
+                    return;
+                }
+                dup2(fd_out, STDOUT_FILENO);
+                close(fd_out);
+                argv[i] = NULL;
+                argv[i+1] = NULL;
+            }
+        }
+
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("Error:");
+        }
+        else if (pid == 0)
+        {
+            if (strcmp(cmd, "env") == 0)
+            {
+                env_builtin();
+            }
+            else if (execve(acmd, argv, NULL) == -1)
+            {
+                perror("Error:");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            int status;
+            if (waitpid(pid, &status, 0) == -1)
+            {
+                perror("Error:");
+            }
+        }
+    }
+    if (cmd != acmd)
+        free(acmd);
 }
