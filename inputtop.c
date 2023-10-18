edaa1f157f3f701459cccde0752cdecccb28a800
asma <asmaa.alhadi100@gmail.com>
2023-10-18 15:41:04 -0700

    /n

--- inputtop.c
+++ inputtop.c
@@ -5,38 +5,31 @@
  * @input: value
  * Return: null
 */
-void inputtop(char *input, size_t s)
-{
+#include "top.h"
+void inputtop(char *input, size_t s) {
 ssize_t checkline;
-const char *delim = " \t\n";
-checkline = getline(&input, &s, stdin);
-if (checkline == -1)
-{
-if (feof(stdin))
-{
-printf("\n");
-exit(EXIT_SUCCESS);
-}
-else
-{
-perror("Error reading input:");
-free(input);
-exit(EXIT_FAILURE);
-}}
-else if (checkline == 1 && input[0] == '\n')
-{
-/* empty input, do nothing*/
-free(input);
-input = NULL;
-return;
-}
-else if (strcmp(input, "exit\n") == 0)
-{
-/* user wants to exit the program */
-free(input);
-input = NULL;
-exit(EXIT_SUCCESS);
-}
-Parse(input, delim);
-free(input);
+ const char *delim = " \t\n";
+ checkline = getline(&input, &s, stdin);
+  if (checkline == -1) {
+   if (feof(stdin)) {
+            printf("\n");
+            exit(EXIT_SUCCESS);
+        } else {
+      perror("Error reading input:");
+      free(input);
+      exit(EXIT_FAILURE);
+    }}
+  else if (checkline == 1 && input[0] == '\n') {
+    /* empty input, do nothing*/
+    free(input);
+    input = NULL;
+    return ;
+  } else if (strcmp(input, "exit\n") == 0) {
+    /* user wants to exit the program */
+    free(input);
+    input = NULL;
+    exit(EXIT_SUCCESS);
+  }
+Parse(input,delim);
+ free(input);
 }
