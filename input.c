b2da7203d8f2c54c5c43f59b2cfac431fd9c63d9
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 04:09:00 +0300

    Update input.c

--- input.c
+++ input.c
@@ -1,6 +1,11 @@
 #include "top.h"
 
-
+/**
+ * empty - function for the user input handling
+ * @input: size of the input
+ * 
+ * Return: int
+*/
 int empty(char *input)
 {
 	if (input == NULL)
@@ -15,7 +20,12 @@ int empty(char *input)
 
 	return (1);
 }
-
+/**
+ * inputtop - function for the user input handling
+ * @s: size of the input
+ * @input: value
+ * Return: null
+*/
 void inputtop(char *input, size_t s, int status) {
 	ssize_t checkline;
 	 const char *delim = " \t\n";
