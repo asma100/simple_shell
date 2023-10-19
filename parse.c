df49201bc1b7f9cb9ec13066cb39487f129575f3
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 03:53:24 +0300

    Create parse.c

---
+++ parse.c
@@ -0,0 +1,68 @@
+#include "top.h"
+/**
+ * Parse - function for the parsing
+ * @input: input from the cmd
+ * @delim: value of the delimeter
+ * Return: the coun number
+*/
+void Parse(char *input, const char *delim)
+{
+char *tok;
+int tok_counter = 0;
+char **arr = NULL;
+int u, i, j;
+char *input_cp = strdup(input);
+if (input_cp == NULL)
+{
+perror("Error allocating memory:");
+free(input);
+return;
+}
+if (input[0] == '\0')
+{
+free(input_cp);
+return;
+}
+tok = strtok(input, delim);
+while (tok != NULL)
+{
+tok_counter++;
+tok = strtok(NULL, delim);
+}
+tok_counter++;
+arr = malloc(sizeof(char *) * tok_counter);
+if (arr == NULL)
+{
+perror("Error allocating memory for arr:");
+free(input);
+free(input_cp);
+return;
+}
+tok = strtok(input_cp, delim);
+for (u = 0; tok != NULL; u++)
+{
+arr[u] = malloc(sizeof(char) * (strlen(tok) + 2));
+if (arr[u] == NULL)
+{
+perror("Error allocating memory for arr[u]:");
+for (j = 0; j < u; j++)
+{
+free(arr[j]);
+}
+free(arr);
+free(input);
+free(input_cp);
+return;
+}
+strcpy(arr[u], tok);
+tok = strtok(NULL, delim);
+}
+arr[u] = NULL;
+topcmd(arr);
+for (i = 0; i < u; i++)
+{
+free(arr[i]);
+}
+free(arr);
+free(input_cp);
+}
