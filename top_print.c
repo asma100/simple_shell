f70288a871e375510e1bd73123151a1fa583418f
asma100 <71214380+asma100@users.noreply.github.com>
2023-10-19 04:10:13 +0300

    Update top_print.c

--- top_print.c
+++ top_print.c
@@ -1,5 +1,9 @@
 #include "top.h"
-
+/**
+ * print_top - function that print
+ * @top: pointer to  printed value
+ * Return: nothing cuz void
+ */
 void print_top(char *top)
 {
 write(STDOUT_FILENO, top, strlen(top));
