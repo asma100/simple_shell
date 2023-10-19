6bbf816fd5b3809ba4945b1994f6d40e400faaca
0xsuda <osama.suliman@protonmail.com>
2023-10-18 20:07:48 -0700

    main betty

--- main.c
+++ main.c
@@ -11,14 +11,14 @@
 
 int main(void)
 {
-	char *input = NULL;
-	int status = 0;
-while (1) 
+char *input = NULL;
+int status = 0;
+while (1)
 {
-if (isatty (STDIN_FILENO))
-			print_top("top$");
-		inputtop(input, sizeof(input), status);
-		status++;
+if (isatty(STDIN_FILENO))
+print_top("top$");
+inputtop(input, sizeof(input), status);
+status++;
 }
 free(input);
 
