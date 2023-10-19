4c5f5079e4ca39a844de1506bac4aeff532628ed
asma <asmaa.alhadi100@gmail.com>
2023-10-19 11:23:51 -0700

    bettyinput

--- input.c
+++ input.c
@@ -1,67 +1,62 @@
 #include "top.h"
-/**                                                                                                                                                                     
- * empty - function for the user input handling                                                                                                                         
- * @input: size of the input                                                                                                                                            
- * Return: int                                                                                                                                                          
+/**
+ * empty - function for the user input handling
+ * @input: size of the input
+ * Return: int
 */
 int empty(char *input)
 {
-	if (input == NULL)
-		return (1);
-
-	while (*input)
-	{
-		if (!isspace(*input))
-			return (0);
-		input++;
-	}
-
-	return (1);
+if (input == NULL)
+return (1);
+while (*input)
+{
+if (!isspace(*input))
+return (0);
+input++;
 }
-/**                                                                                                                                                                     
- * inputtop - function for the user input handling                                                                                                                      
- * @s: size of the input                                                                                                                                                
- * @input: value                                                                                                                                                        
- * @status:0,1                                                                                                                                                          
- * Return: null                                                                                                                                                         
+return (1);
+}
+/**
+ * inputtop - function for the user input handling
+ * @s: size of the input
+ * @input: value
+ * @status:0,1
+ * Return: null
 */
-void inputtop(char *input, size_t s, int status) {
-	ssize_t checkline;
-	 const char *delim = " \t\n";
-
-	checkline = getline(&input, &s, stdin);
-	if (checkline == -1)
-	{
-		free(input);
-		exit(EXIT_SUCCESS);
-	}
-	else if (strcmp(input, "exit\n") == 0)
-	{
-		free(input);
-		if (status == 0)
-			exit(0);
-		else
-		{
-			
-			exit (2);
-		}
-	}
-	else if (strcmp(input, "env\n") == 0)
-	{
-		env_builtin();
-		free(input);
-		exit (0);
-	}
-	else
-	{
-		if (empty(input) == 1)
-		{
-			free(input);
-			return;
-		}
-
-
-		Parse(input,delim);
-	}
-	free(input);
+void inputtop(char *input, size_t s, int status)
+{
+ssize_t checkline;
+const char *delim = " \t\n";
+checkline = getline(&input, &s, stdin);
+if (checkline == -1)
+{
+free(input);
+exit(EXIT_SUCCESS);
+}
+else if (strcmp(input, "exit\n") == 0)
+{
+free(input);
+if (status == 0)
+exit(0);
+else
+{
+exit(2);
+}
+}
+else if (strcmp(input, "env\n") == 0)
+{
+env_builtin();
+free(input);
+exit(0);
+}
+else
+{
+if (empty(input) == 1)
+{
+free(input);
+return;
+}
+Parse(input, delim);
+}
+free(input);
 }
