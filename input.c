d0ed43a9d732f496cd848eebc862fafa72450509
0xsuda <osama.suliman@protonmail.com>
2023-10-20 19:40:08 -0700

    betty update

--- input.c
+++ input.c
@@ -59,7 +59,7 @@ Parse(input, delim);
 free(input);
 }
 /**
- * newline - function fornewline
+ * comm - function fornewline
  *
  * @input: value
  *
@@ -68,9 +68,10 @@ free(input);
 char *comm(char *input)
 {
 int i;
-if (input[0] == '#') {
+if (input[0] == '#')
+{
 /* Input is a comment, return empty string */
-return "";
+return ("");
 }
 i = 0;
 while (input[i] != '\0')
@@ -82,5 +83,5 @@ break;
 }
 i++;
 }
-return input;
+return (input);
 }
